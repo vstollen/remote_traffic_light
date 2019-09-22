1ec3ba2cb5e0d52a1d0eeb80d22237755caa4e4a
Vincent <vstollen@gmail.com>
2019-09-22 14:05:07 +0200

    Fix python3 compatibility
    Add explicid utf-8 encoding for sending messages

--- traffic_light_client.py
+++ traffic_light_client.py
@@ -3,14 +3,14 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_adress = ('localhost', 10000)
+server_adress = ('raspberrypi.lan', 10000)
 print('Connecting to %s on port %s' % server_adress)
 sock.connect(server_adress)
 
 try:
 	message = sys.argv[1]
 	print('Sending: ', message)
-	sock.sendall(message)
+	sock.sendall(message.encode('utf-8'))
 
 finally:
 	print('Closing Socket')
